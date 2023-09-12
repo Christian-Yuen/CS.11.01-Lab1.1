cd43728f77e92534e785139721441dc694e75335
christian <christiany2026@student.cis.edu.hk>
2023-09-12 17:19:21 +0800

    IDE 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
-
-    public static void main(String[] args) {
-        
+     public static void main (String[] args){
+         Scanner scanner = new Scanner (System.in);
+         System.out.println("Enter Your Age");
+         int age = scanner.nextInt();
+         int date = age/2;
+         int dateage = date+7;
+         System.out.println("Your dating range is "+dateage+" and up");
+     }
     }
-}
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Pages {
-
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        int page = 100-age;
+        System.out.println("A "+age+" years old should read "+page+" before giving up on a book");
     }
 }
